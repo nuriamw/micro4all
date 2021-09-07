83d7105ee53ce7be40d3b0d86e1b3cf4e84a545c
nuriamw <nuria.m.w@gmail.com>
2021-09-07 15:54:35 +0200

    Reoxygen comments added to Levene.test.alpha

--- R/Levene.R
+++ R/Levene.R
@@ -1,14 +1,37 @@
-
-
-
-
+#'Levene's test for multiple variables
+#'
+#'
+#'@description Test homogeneity of variance across groups, looping across
+#'  multiple variables (e.g. alpha diversity indexes).
+#'
+#'@param data a data frame, columns corresponding to variables (e.g. alpha diversity indexes) and rows
+#'  corresponding to samples. Further columns should be included with metadata.
+#'  This is used in argument \code{Formula}.
+#'@param numberOfIndexes Integer corresponding to the number of indexes to
+#'  analyze. This will be take as column numbers by the function.
+#'@param formula  Metadata group name. This will group samples according to a
+#'  metadata column and specify the model.
+#'@param ... Further arguments to be passed to \code{leveneTest}.
+#'
+#'@return  returns a data frame with results from \code{leveneTest} from package \code{car}
+#'  for each variable (determined by \code{numberOfIndexes}).Further details can
+#'  be found in \code{?leveneTest}
+#'
+#'
+#'
+#'@export
+#'
+#' @examples
+#'
+#' levene_rhizo <- Levene.test.alpha(alpha_indexes, 4, "Management")
+#'
 Levene.test.alpha <- function (data, numberOfIndexes,formula,...) {
   ##Create names
 
   res.levene<- NULL
   indexColumn <- NULL
   for (i in 1:numberOfIndexes){
-    lev <- leveneTest(data[,i] ~ data[,formula], data = data,...)
+    lev <- car::leveneTest(data[,i] ~ data[,formula], data = data,...)
     res.levene<- rbind(res.levene,lev)
     indexColumn <- rbind(indexColumn, data.frame(rep(colnames(data)[i],nrow(lev))))
 
