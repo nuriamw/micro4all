722d4b1467c387469282ccc85156be5b45fdf99e
nuriamw <nuria.m.w@gmail.com>
2021-09-07 15:30:35 +0200

    Some modifications added to Tukey. For example,... argument (just to past to TukeyHSD or HSD.test)

--- R/Tuckey.R
+++ R/Tuckey.R
@@ -1,31 +1,42 @@
 #'Multiple comparisons for multiple variables: balanced and unbalanced.
 #'
-#'@description Performs multiple comparisons and loops over variables. This
-#'  function is based on \code{TukeyHSD} and \code{HSD.test} from packages
-#'  \code{stats} and \code{agricolae} for multiple comparisons. These are
-#'  subsequently based on \code{aov} from  \code{stats} for model design.
+#'@description Performs multiple comparisons in each variable. Variables should
+#'  be columns in \code{data}. This function is based on \code{TukeyHSD} and
+#'  \code{HSD.test} from packages \code{stats} and \code{agricolae} for multiple
+#'  comparisons. These are subsequently based on \code{aov} from \code{stats}
+#'  for model design.
 #'
 #'@param data a data frame, columns corresponding to indexes and rows
 #'  corresponding to samples. Further columns should be included with metadata.
-#'  This is used for Formula as a grouping factor.
+#'  This is used in argument \code{Formula}.
 #'@param numberOfIndexes Integer corresponding to the number of indexes to
 #'  analyze. This will be take as column numbers by the function.
 #'@param formula Metadata group name. This will group samples according to a
 #'  metadata column and specify the model.
-#'@param balanced
+#'@param balanced whether to perform a balanced (TRUE) or unbalanced (FALSE)
+#'  test
 #'
-#'@param ... Further arguments passed to \code{aov}, \code{HSD.test} or \code{TukeyHSD}
+#'@param ... Further arguments to be passed to \code{HSD.test} or
+#'  \code{TukeyHSD}
 #'
-#'@return
+#'@return Returns a data frame with as many comparisons as groups, performed for
+#'  all variables (determined by \code{numberOfIndexes}). Details about
+#'  parameters should be check with \code{?TukeyHSD} and \code{?HSD.test}. If
+#'  \code{balanced=FALSE}, it returns the \code{comparison} element of
+#'  \code{HSD.test}.
 #'@export
 #'
 #' @examples
+#'
+#' tukey_balanced<- Tukey.test(alpha_indexes_rhizo, numberOfIndexes = 4, formula = "Management", balanced=TRUE)
+#' tukey_unbalanced<- Tukey.test(alpha_indexes_rhizo, numberOfIndexes = 4, formula = "Management", balanced=FALSE)
+#'
 Tukey.test <- function (data, numberOfIndexes,formula, balanced,...) {
 
   tukey.results <- NULL
   indexColumn <- NULL
   if (balanced==TRUE){  for (i in 1:numberOfIndexes){
-    tukey <- stats::TukeyHSD(stats::aov(data[,i] ~ data[,formula], data = data,...),...)[[1]]
+    tukey <- stats::TukeyHSD(stats::aov(data[,i] ~ data[,formula], data = data),...)[[1]]
     tukey.results <- rbind(tukey.results, tukey)
     indexColumn <- rbind(indexColumn, data.frame(rep(colnames(data)[i],nrow(tukey))))
 
@@ -37,7 +48,7 @@ Tukey.test <- function (data, numberOfIndexes,formula, balanced,...) {
     tukey.results.unbalanced<- NULL
     for (i in 1:numberOfIndexes){
       formuloca <- paste(colnames(data)[i], "~",formula, sep=" ")
-      hsd<- agricolae::HSD.test(stats::aov(as.formula(formuloca), data=data,...), trt=formula, group=FALSE, unbalanced=TRUE, console=FALSE,..getNamespace())[["comparison"]]
+      hsd<- agricolae::HSD.test(stats::aov(as.formula(formuloca), data=data), trt=formula, group=FALSE, unbalanced=TRUE, console=FALSE,..getNamespace())[["comparison"]]
       tukey.results.unbalanced <- rbind(tukey.results.unbalanced,hsd)
       indexColumn <- rbind(indexColumn, data.frame(rep(colnames(data)[i],nrow(hsd))))
 
