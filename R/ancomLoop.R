603a2ac773a7de8e05eec3cdea300802206723ed
nuriamw <nuria.m.w@gmail.com>
2022-04-25 20:01:25 +0200

    remove ancom corrected abundances calculation from function

--- R/ancomLoop.R
+++ R/ancomLoop.R
@@ -3,12 +3,9 @@
 #'   having multiple groups to compare, it allows the user to automatically loop
 #'   over them and computes all possible combinations. Along with
 #'   \link[ANCOMBC]{ancombc} results, it appends to each table the taxonomy
-#'   classification and computes the bias-adjusted abundance (expressed as
-#'   logarithm), as well as the standard deviation for each group.
-#'
-#'
+#'   classification.
 #' @param input_object_phyloseq \link[phyloseq]{phyloseq-class}. For more
-#'   details, check \link[phyloseq]{distance} funtion.
+#'   details, check \link[phyloseq]{distance} function.
 #' @param grouping Character string. This is used as metadata variable for
 #'   reordering factors (which allows the function to loop over groups). It is
 #'   also passed to \code{group} and \link{formula} arguments in
@@ -23,7 +20,7 @@
 #'
 #' @return Returns a list with as many arguments as groups are contained in
 #'   metadata "grouping" variable. Each element is named after the group which
-#'   is compared with all others and correspond to a data frame that inlcudes
+#'   is compared with all others and correspond to a data frame that includes
 #'   \link[ANCOMBC]{ancombc} parameters and the bias-adjusted abundance.
 #'   In the process, it prints the groups that are being compared (first group against others).
 #'
@@ -33,13 +30,15 @@
 #' @examples
 #'
 #'ancom_location<- ancomloop(location_phyloseq,grouping="location",
-#'ancom.options=list(p_adj_method ="holm"), out.unclassified=TRUE, tax.level="Genus")
+#'ancom.options=list(p_adj_method ="holm"),
+#'out.unclassified=TRUE, tax.level="Genus")
+#'
+#'
+#'
+#'
+#'
 
-#'
-#'
-#'
-#'
-#'
+
 ancomloop <-  function (input_object_phyloseq, grouping,
                         ancom.options, out.unclassified=FALSE, tax.level){
 
@@ -128,34 +127,9 @@ ancomloop <-  function (input_object_phyloseq, grouping,
     # BIND TAXONOMY AND CORRECTED ABUNDANCES
     taxa <- phyloseq::tax_table(input_object_phyloseq)
 
-    #GET CORRECTED ABUNDANCE FOR ANCOM
-    samp_frac <-  response_out$samp_frac
-    # Replace NA with 0
-    samp_frac[is.na(samp_frac)] <-  0
-
-    # Add pesudo-count (1) to avoid taking the log of 0
-    log_obs_abn  <-  log(microbiome::abundances(input_object_phyloseq) + 1)
-    #Adjust the log observed abundances
-    log_obs_abn_adj  <-  t(t(log_obs_abn) - samp_frac)
-    t_log_abn_adj <- t(log_obs_abn_adj)
-
-
-    #Calculate mean and SD
-    mean <- stats::aggregate(t_log_abn_adj, by=list(mt[[grouping]]), FUN=mean)%>% tibble::column_to_rownames("Group.1") %>% t()%>%
-      as.data.frame()
-
-    #Calculate OTU SD  based on grouping factor (e.g., PLOT) and change colnames
-    SD <- stats::aggregate(t_log_abn_adj, by=list(mt[[grouping]]), FUN=stats::sd)%>% tibble::column_to_rownames("Group.1")  %>% t()  %>%
-      as.data.frame() %>% dplyr::rename_with(.fn= ~paste0(colnames(mean), "SD"))
-
-    mean <- mean %>% dplyr::rename_with(.fn= ~paste0(colnames(mean),"MeanLogAbun"))
-    #Merge mean abundance, SD and taxonomy.
-    merged_table <- merge(taxa, mean, by=0) %>%tibble::column_to_rownames("Row.names") %>%
-      merge(SD, by=0) %>% tibble::column_to_rownames("Row.names")
-
     #################### Merge table with ANCOM Results #########################
 
-    table_ancom_log <- merge(merged_table, tabla_ancom_sorted, by=0) %>% tibble::column_to_rownames("Row.names")
+    table_ancom_log <- merge(taxa, tabla_ancom_sorted, by=0) %>% tibble::column_to_rownames("Row.names")
 
     ##IF out.unclassified set to TRUE, filter unclassified taxa at taxonomical level set by tax.out
     if (isTRUE(out.unclassified)){
